db3b1cfed7756d51c9ce0a357dcdf0e3f414ebd7
kayzere <katizorek@gmail.com>
2020-02-15 20:21:00 +0100

    Add files via upload

--- views/albums_new.php
+++ views/albums_new.php
@@ -4,8 +4,8 @@
 <form method="post" action="/index.php/gallery/albums_create">
   <div class="form-group">
     <label for="album_name">Nom de l'album</label>
-    <input type="text" class="form-control" value="<?=set_value('album_name ou photo_name');?>" name="album_name" id="album_name" autofocus>
+    <input type="text" class="form-control" id="album_id" name="album_name" value="<?=set_value('album_name');?>" autofocus>
   </div>
-  <button type="submit" value="<?=set_value('album_name ou photo_name');?>" class="btn btn-success">Créer mon album</button>
+  <button type="submit" class="btn btn-success">Créer mon album</button>
   <a href="/index.php" class="btn btn-danger">Annuler</a>
 </form>
